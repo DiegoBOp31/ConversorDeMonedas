defcb58fd5ab6d75f1dac7ac1f79ce7f57e38f8c
Diego Rosales Rebollar <diegorila-31@hotmail.com>
2025-05-20 18:13:06 -0600

    Construcción de petición a la api y menú de selección

--- src/ConsultaMoneda.java
+++ src/ConsultaMoneda.java
@@ -1,3 +1,74 @@
+import com.google.gson.Gson;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class ConsultaMoneda {
 
+    public Moneda buscarCambio(int eleccionMenu, double cantidad){
+
+        URI direccion = null;
+        switch (eleccionMenu) {//Aquí sólo construimos la URL para hacer el cambio
+            case 1:
+                 direccion = URI
+                        .create("https://v6.exchangerate-api.com/v6/4c64c2279ba23174e7c55f28/USD/MXN/" + cantidad);
+                break;
+            case 2:
+                direccion = URI
+                        .create("https://v6.exchangerate-api.com/v6/4c64c2279ba23174e7c55f28/MXN/USD/" + cantidad);
+                break;
+            case 3:
+                direccion = URI
+                        .create("https://v6.exchangerate-api.com/v6/4c64c2279ba23174e7c55f28/USD/ARS/" + cantidad);
+                break;
+            case 4:
+                direccion = URI
+                        .create("https://v6.exchangerate-api.com/v6/4c64c2279ba23174e7c55f28/ARS/USD/" + cantidad);
+                break;
+            case 5:
+                direccion = URI
+                        .create("https://v6.exchangerate-api.com/v6/4c64c2279ba23174e7c55f28/USD/BRL/" + cantidad);
+                break;
+            case 6:
+                direccion = URI
+                        .create("https://v6.exchangerate-api.com/v6/4c64c2279ba23174e7c55f28/BRL/USD/" + cantidad);
+                break;
+            case 7:
+                direccion = URI
+                        .create("https://v6.exchangerate-api.com/v6/4c64c2279ba23174e7c55f28/USD/COP/" + cantidad);
+                break;
+            case 8:
+                direccion = URI
+                        .create("https://v6.exchangerate-api.com/v6/4c64c2279ba23174e7c55f28/COP/USD/" + cantidad);
+                break;
+            default:
+                System.out.println("Esa opción no es válida");
+                return null;
+        }
+
+
+        //Crea un "cliente HTTP", es decir, algo que puede hacer peticiones a internet
+        HttpClient client = HttpClient.newHttpClient();
+        /*
+         Se prepara la solicitud para pedir datos desde la dirección web.
+        .uri (direccion) = indica a dónde vamos a conectarnos.
+        .build() = termina de construir la solicitud.
+         */
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(direccion)
+                .build();
+
+        try{
+            //Manda esta solicitud al servidor y guarda la respuesta (como texto) en una variable llamada response
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+            //fromJson(response.body()) convierte el texto JSON que nos dio la API en un objeto Java llamado Moneda
+            return new Gson().fromJson(response.body(), Moneda.class);
+        }catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner lectura = new Scanner(System.in);
+        ConsultaMoneda consulta = new ConsultaMoneda();
+        System.out.println("**************************************************");
+        System.out.println("Sea bienvenid@ al Conversor de Moneda =]");
+        System.out.println("Elija una opción válida:");
+        System.out.println("\n1) Dólar =>> Peso mexicano");
+        System.out.println("2) Peso mexicano =>> Dólar");
+        System.out.println("3) Dólar =>> Peso argentino");
+        System.out.println("4) Peso argentino =>> Dólar");
+        System.out.println("5) Dólar =>> Real brasileño");
+        System.out.println("6) Real brasileño =>> Dólar");
+        System.out.println("7) Dólar =>> Peso colombiano");
+        System.out.println("8) Peso colombiano =>> Dólar");
+        System.out.println("9) Salir");
+        System.out.println("**************************************************");
     }
 }
